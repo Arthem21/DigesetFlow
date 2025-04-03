1cf678f61a70ac44e64a634991512633d5c8dc22
CrisGLpez <176536989+Arthem21@users.noreply.github.com>
2025-04-02 23:12:52 -0400

    Delete Components directory

--- Components/Modelos/Usuario.cs
+++
@@ -1,8 +0,0 @@
-public abstract class Ususario{
-
-    public int ID{get; set;}=0;
-    public string Cedula {get; set;}= string.Empty;
-    public string Clave {get; set;}= string.Empty;
-    public string Tipo {get; set;}= string.Empty;
-    
-}
